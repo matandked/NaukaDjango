129b3dae4380a7015f229932e7fb33938087ec36
marcinkawka <kawka.marcin@gmail.com>
2016-10-08 23:25:00 +0200

    posty sie juz wyswietlaja jak wpiszesz localhost:8000 a przez panel admina mozna je dodawac

--- SentinelSite/blogasek/views.py
+++ SentinelSite/blogasek/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    #pobieramy posty jako QuerySet (to takie obiekty zassane z bazy danych)
+    #Post zdefiniowalismy w modelach
+    posty = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blogasek/post_list.html', {'posts':posty})
+    
