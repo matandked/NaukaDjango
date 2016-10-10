f16e9f08b61e3971b8255c0d40e5defc27f9682f
marcinkawka <kawka.marcin@gmail.com>
2016-10-10 21:39:29 +0200

    dodałem reużywalność kaawałka templejtu

--- SentinelSite/blogasek/urls.py
+++ SentinelSite/blogasek/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     #widok post_list do pustego URL
     url(r'^$', views.post_list, name='post_list'),
+    # /post/numer przezkaze numer do <pk> i wysle do post_detail
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- SentinelSite/blogasek/views.py
+++ SentinelSite/blogasek/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,6 @@ def post_list(request):
     posty = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blogasek/post_list.html', {'posts':posty})
     
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogasek/post_detail.html', {'post': post})
