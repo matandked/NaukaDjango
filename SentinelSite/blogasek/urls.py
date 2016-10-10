10332e05f230bf11518ecb314a664d0d16d473de
marcinkawka <kawka.marcin@gmail.com>
2016-10-10 21:59:46 +0200

    edycja postow

--- SentinelSite/blogasek/urls.py
+++ SentinelSite/blogasek/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     # /post/numer przezkaze numer do <pk> i wysle do post_detail
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
